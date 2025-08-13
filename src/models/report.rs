5a5557669bad19fe907b35d85fbc2e02d774094b
jhenrique04 <jhenriquevs.04@gmail.com>
2025-08-13 11:17:45 -0300

    fix: remove unused imports

--- src/models/report.rs
+++ src/models/report.rs
@@ -1,8 +1,6 @@
 use serde::{Deserialize, Serialize};
 use sqlx::Type;
-use time::OffsetDateTime;
 use utoipa::ToSchema;
-use uuid::Uuid;
 
 #[derive(Debug, Clone, Copy, Serialize, Deserialize, ToSchema, Type)]
 #[sqlx(type_name = "VARCHAR")]
@@ -13,15 +11,4 @@ pub enum ReportReason {
     Harassment,
     Spam,
     Other,
-}
-
-#[derive(Debug, Serialize, sqlx::FromRow, ToSchema)]
-pub struct Report {
-    pub id: Uuid,
-    pub reporter_key: String,
-    pub target_type: String,
-    pub target_id: String,
-    pub reason_code: ReportReason,
-    pub description: Option<String>,
-    pub created_at: OffsetDateTime,
 }
